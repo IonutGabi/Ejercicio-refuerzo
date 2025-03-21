417af1bb44d684b4ab9cda8c8f4a6c4f546ccdb2
IonutGabi <birsanionut@gmail.com>
2025-03-21 11:15:59 +0100

    Mejorando y arreglando las implementaciones de los ejercicios de refuerzo

--- src/main.ts
+++ src/main.ts
@@ -32,25 +32,35 @@ if (button && button instanceof HTMLButtonElement) {
 
  Total a pagar = Horas trabajadas × Tarifa por hora */
 
-const calculaTarifa = (numero: number) => {
-  const tarifaPorHora = 2.5;
-  const resultado = numero * tarifaPorHora;
-  console.log(`El total a pagar es ${resultado}`);
-};
+let horasTrabajadas = 0;
 
-const workedHoursInput = document.querySelector(".horasTrabajadas");
-let horasTrabajadas: number = 0;
-if (workedHoursInput && workedHoursInput instanceof HTMLInputElement) {
-  workedHoursInput.addEventListener("input", () => {
-    horasTrabajadas = parseInt(workedHoursInput.value);
-  });
+const handleHorasTrabajadas = (): void => {
+  const horasTrabajadasInput = document.querySelector(".horasTrabajadas");
+
+  if (
+    horasTrabajadasInput &&
+    horasTrabajadasInput instanceof HTMLInputElement
+  ) {
+    horasTrabajadasInput.addEventListener("input", () => {
+      horasTrabajadas = parseInt(horasTrabajadasInput.value);
+    });
+  }
+};
+const handleCalculoPagar = (): void => {
   const calculoPagar = document.querySelector(".calculoPagar");
   if (calculoPagar && calculoPagar instanceof HTMLButtonElement) {
     calculoPagar.addEventListener("click", () =>
       calculaTarifa(horasTrabajadas)
     );
   }
-}
+};
+const calculaTarifa = (numero: number) => {
+  const tarifaPorHora = 1.5;
+  const resultado = numero * tarifaPorHora;
+  console.log(`El total a pagar es ${resultado}`);
+};
+handleHorasTrabajadas();
+handleCalculoPagar();
 
 //  4. Queremos mostrar el día sin IVA, desarrolla una aplicación en la que le de un precio, calcule su IVA y me muestre el precio sin IVA y cuanto me ahorro en IVA, suponiendo que es un 21%.
 
@@ -66,13 +76,24 @@ calculoIva(20);
 
 // 5. Igual que el 4, pero eligiendo el tipo de IVA. Puedes crearte un select, con distintos tipos de iva y que se haga el cálculo.
 
+const handleSelection = (
+  domElement: HTMLSelectElement,
+  callback: (iva: number) => void
+): void => {
+  domElement.addEventListener("change", async () => {
+    const valorSeleccionado = parseInt(domElement.value);
+    const iva = 1 + valorSeleccionado / 100;
+    callback(iva);
+  });
+};
+
+const precioConIva = (numero: number, iva: number): number => numero * iva;
+
 const calculoIvaDinamico = (numero: number) => {
   const select = document.getElementById("ivaSelect");
   if (select && select instanceof HTMLSelectElement) {
-    select.addEventListener("change", () => {
-      const valorSeleccionado = parseInt(select.value);
-      const iva = 1 + valorSeleccionado / 100;
-      const resultado = numero * iva;
+    handleSelection(select, (iva) => {
+      const resultado = precioConIva(numero, iva);
       const ahorro = Math.round(resultado - numero);
       console.log(
         `El precio sin IVA es ${numero} €, con IVA es ${resultado} € y en IVA te ahorras ${ahorro} €`
@@ -91,21 +112,34 @@ calculoIvaDinamico(20);
 > Si los dias que faltan son igual a 0 y las horas mayores o igual a 0 los días que faltan son 7 y si no cumple esta condición
 > el valor de días que faltan será el valor del paso anterior. */
 
+const calculoDiasRestantes = (diaActual: number): number =>
+  (5 - diaActual + 7) % 7;
+
+const calculoHorasRestantes = (
+  diaActual: number,
+  horaActual: number
+): number => {
+  let diasRestantes = calculoDiasRestantes(diaActual);
+
+  const horasRestantesHoy = 24 - horaActual;
+  const horasRestantes = diasRestantes * 24 + horasRestantesHoy;
+
+  return horasRestantes;
+};
+
 const calculosDiasHastaElViernes = (numero: number) => {
   const hoy = new Date(numero);
   const diaActual = hoy.getDay();
   const horaActual = hoy.getHours();
 
-  let diasRestantes = (5 - diaActual + 7) % 7;
+  let diasRestantes = calculoDiasRestantes(diaActual);
 
   if (diasRestantes === 0 && horaActual >= 0) {
     diasRestantes = 7;
   } else {
     diasRestantes;
   }
-
-  const horasRestantesHoy = 24 - horaActual;
-  const horasRestantes = diasRestantes * 24 + horasRestantesHoy;
+  const horasRestantes = calculoHorasRestantes(diaActual, horaActual);
 
   console.log(
     `Faltan aproximadamente ${diasRestantes} días y ${horasRestantes} horas para el viernes`
@@ -114,11 +148,20 @@ const calculosDiasHastaElViernes = (numero: number) => {
 
 calculosDiasHastaElViernes(Date.now());
 
-const fechaEjemplo = new Date(2025, 2, 21).getTime();
-calculosDiasHastaElViernes(fechaEjemplo);
-
 /* 7. Introduce un número por un input, y que te muestre la sucesión de [Fibonacci](https://es.wikipedia.org/wiki/Sucesi%C3%B3n_de_Fibonacci) según el número introducido.
 Por ejemplo, si introduzco un 8, la salida tiene que ser: `0, 1, 1, 2, 3, 5, 8, 13`*/
+
+const handleInputFibonacci = () => {
+  const stringFibonacci = document.getElementById("fibonacci");
+  if (stringFibonacci && stringFibonacci instanceof HTMLInputElement) {
+    stringFibonacci.addEventListener("keydown", (e) => {
+      const numeroParaFibonacci = parseInt(stringFibonacci.value);
+      if (e.key === "Enter") {
+        calculoSerieFibonacci(numeroParaFibonacci);
+      }
+    });
+  }
+};
 
 const calculoSerieFibonacci = (numero: number) => {
   const fibonacci = [0, 1];
@@ -128,20 +171,7 @@ const calculoSerieFibonacci = (numero: number) => {
   }
   console.log(fibonacci);
 };
-
-const numeroEnStringParaFibonacci = document.getElementById("fibonacci");
-
-if (
-  numeroEnStringParaFibonacci &&
-  numeroEnStringParaFibonacci instanceof HTMLInputElement
-) {
-  numeroEnStringParaFibonacci.addEventListener("keydown", (e) => {
-    const numeroParaFibonacci = parseInt(numeroEnStringParaFibonacci.value);
-    if (e.key === "Enter") {
-      calculoSerieFibonacci(numeroParaFibonacci);
-    }
-  });
-}
+handleInputFibonacci();
 
 /*
  8. Realiza un programa que pida una hora y muestre un mensaje.
@@ -165,30 +195,42 @@ muestraMensajeSegunHora(5);
 /* 9. Una asignatura está compuesta por cuatro examenes, para aprobar hay que sacar más de un 3 en cada uno de ellos, y la media de los 4 examenes debe tener en total una puntuación de más de 5 (el examen se puntua de 0 a 10), crea un programa en el que introduzcas cuatro notas y te diga si eres apto o no.
 Te pasamos un posible html como referencia (puedes mostrar el resultado por consola o por pantalla):*/
 
+const handleInputNotas = (domElement: NodeList, arrayNotas: number[]) => {
+  domElement.forEach((nota) => {
+    if (nota && nota instanceof HTMLInputElement) {
+      const transformStringToNumber = parseInt(nota.value);
+      arrayNotas.push(transformStringToNumber);
+    }
+  });
+};
+
+const calculoMedia = (arrayNotas: number[]): number => {
+  const suma = arrayNotas.reduce((acc, nota) => acc + nota, 0);
+  const media = Math.round(suma / arrayNotas.length);
+
+  return media;
+};
+
+const handleMessage = (media: number) => {
+  const parrafo = document.getElementById("mensaje");
+
+  if (parrafo && parrafo instanceof HTMLParagraphElement) {
+    media > 5
+      ? (parrafo.textContent = "Apto")
+      : (parrafo.textContent = "No Apto");
+  }
+};
+
 const calcularNota = () => {
   const recogidaDeNotas = document.querySelectorAll("input[type='number']");
 
   if (recogidaDeNotas && recogidaDeNotas instanceof NodeList) {
     let arrayDeNotas: number[] = [];
-    recogidaDeNotas.forEach((nota) => {
-      if (nota && nota instanceof HTMLInputElement) {
-        const transformStringToNumber = parseInt(nota.value);
-        arrayDeNotas.push(transformStringToNumber);
-      }
-    });
-
-    const suma = arrayDeNotas.reduce((acc, nota) => acc + nota, 0);
-    const media = suma / arrayDeNotas.length;
-
-    const parrafo = document.getElementById("mensaje");
-    if (parrafo && parrafo instanceof HTMLParagraphElement) {
-      media > 5
-        ? (parrafo.textContent = "Apto")
-        : (parrafo.textContent = "No Apto");
-    }
+    handleInputNotas(recogidaDeNotas, arrayDeNotas);
+    const media = calculoMedia(arrayDeNotas);
+    handleMessage(media);
   }
 };
-
 const botonCalcularNota = document.querySelector(".calcularNota");
 if (botonCalcularNota && botonCalcularNota instanceof HTMLButtonElement) {
   botonCalcularNota.addEventListener("click", calcularNota);
@@ -201,39 +243,33 @@ if (botonCalcularNota && botonCalcularNota instanceof HTMLButtonElement) {
 - Notable 7, 8, 9
 - Sobresaliente => 10 */
 
+const handleParagraph = (media: number) => {
+  const parrafo = document.getElementById("mensaje");
+  if (parrafo && parrafo instanceof HTMLParagraphElement) {
+    let mensaje: string = "";
+    if (media >= 0 && media <= 4) {
+      mensaje = "Insuficiente";
+    } else if (media >= 5 && media <= 6) {
+      mensaje = "Suficiente";
+    } else if (media >= 7 && media <= 9) {
+      mensaje = "Notable";
+    } else if (media === 10) {
+      mensaje = "Sobresaliente";
+    }
+    parrafo.textContent = mensaje;
+  }
+};
+
 const mostrarNota = () => {
   const recogidaDeNotas = document.querySelectorAll("input[type='number']");
 
   if (recogidaDeNotas && recogidaDeNotas instanceof NodeList) {
     let arrayDeNotas: number[] = [];
-    recogidaDeNotas.forEach((nota) => {
-      if (nota && nota instanceof HTMLInputElement) {
-        const transformStringToNumber = parseInt(nota.value);
-        arrayDeNotas.push(transformStringToNumber);
-      }
-    });
-
-    const suma = arrayDeNotas.reduce((acc, nota) => acc + nota, 0);
-    const media = Math.round(suma / arrayDeNotas.length);
-
-    const parrafo = document.getElementById("mensaje");
-
-    if (parrafo && parrafo instanceof HTMLParagraphElement) {
-      let mensaje: string = "";
-      if (media >= 0 && media <= 4) {
-        mensaje = "Insuficiente";
-      } else if (media >= 5 && media <= 6) {
-        mensaje = "Suficiente";
-      } else if (media >= 7 && media <= 9) {
-        mensaje = "Notable";
-      } else if (media === 10) {
-        mensaje = "Sobresaliente";
-      }
-      parrafo.textContent = mensaje;
-    }
+    handleInputNotas(recogidaDeNotas, arrayDeNotas);
+    const media = calculoMedia(arrayDeNotas);
+    handleParagraph(media);
   }
 };
-
 const botonMostrarNota = document.querySelector(".calcularNota");
 if (botonMostrarNota && botonMostrarNota instanceof HTMLButtonElement) {
   botonMostrarNota.addEventListener("click", mostrarNota);
@@ -280,28 +316,29 @@ const mostrarTablaMultiplicar = (numero: number) => {
 
 mostrarTablaMultiplicar(8);
 
-/*14. Escribe un programa que me diga si un número que introducido por teclado es primo o no.
+/* 14. Escribe un programa que me diga si un número que introducido por teclado es primo o no.
 > Recuerda que un número primo es aquel que es mayor que uno y tiene únicamente dos divisores: él mismo y el 1 */
 
 const muestraSiEsPrimo = (numero: number) => {
-  if (numero < 2) {
-    return false;
-  }
+  let contador = 0;
+  let i = 1;
 
-  for (let i = 2; i <= Math.sqrt(numero); i++) {
-    if (numero % i === 0) {
-      return false;
+  while (i <= numero) {
+    if (numero <= 1) {
+      console.error("El numero debe ser mayor de 1");
     }
+    if (numero % i === 0) {
+      contador++;
+    }
+    i++;
   }
-  return true;
+  contador === 2
+    ? console.log(`${numero} es primo`)
+    : console.log(`${numero} no es primo`);
 };
 
 const numero = parseInt(prompt("Introduce un número:") || "0");
-if (muestraSiEsPrimo(numero)) {
-  console.log(`El número ${numero} es primo`);
-} else {
-  console.log(`El número ${numero} no es primo`);
-}
+muestraSiEsPrimo(numero);
 
 /* 15. Escribe un programa que te calcula el número factorial de un numero (no uses librerías externas).
   > Un factorial es un número que es el producto de todos los números enteros desde 1 a N
@@ -321,35 +358,46 @@ calcularFactorial(5);
 Te pasamos un posible html como referencia (puedes mostrar el resultado por consola o por pantalla):
 */
 
+const recogerNumeros = (domElement: NodeList, arrayNotas: number[]) => {
+  domElement.forEach((numero) => {
+    if (numero && numero instanceof HTMLInputElement) {
+      const transformStringToNumber = parseInt(numero.value);
+      arrayNotas.push(transformStringToNumber);
+    }
+  });
+};
+
+const obtenerMinimo = (arrayNumeros: number[]): number =>
+  Math.min(...arrayNumeros);
+const obtenerMaximo = (arrayNumeros: number[]): number =>
+  Math.max(...arrayNumeros);
+
+const calcularModa = (numero: number, arrayNumero: number[]): number => {
+  for (let i = 0; i < arrayNumero.length; i++) {
+    if (arrayNumero.filter((num) => num === arrayNumero[i]).length > 1) {
+      numero = arrayNumero[i];
+    }
+  }
+  return numero;
+};
+
 const calcularDatosFamilia = () => {
   const recogidaNumeros = document.querySelectorAll("input[type='number']");
 
   if (recogidaNumeros && recogidaNumeros instanceof NodeList) {
     let arrayDeNumeros: number[] = [];
     let moda: number = 0;
-    recogidaNumeros.forEach((numero) => {
-      if (numero && numero instanceof HTMLInputElement) {
-        const transformStringToNumber = parseInt(numero.value);
-        arrayDeNumeros.push(transformStringToNumber);
-      }
-    });
+    recogerNumeros(recogidaNumeros, arrayDeNumeros);
 
-    const minimo = Math.min(...arrayDeNumeros);
-    const maximo = Math.max(...arrayDeNumeros);
+    const minimo = obtenerMinimo(arrayDeNumeros);
+    const maximo = obtenerMaximo(arrayDeNumeros);
 
-    const suma = arrayDeNumeros.reduce((acc, num) => acc + num, 0);
-    const media = Math.round(suma / arrayDeNumeros.length);
+    const media = calculoMedia(arrayDeNumeros);
+    const laModa = calcularModa(moda, arrayDeNumeros);
 
-    for (let i = 0; i < arrayDeNumeros.length; i++) {
-      if (
-        arrayDeNumeros.filter((num) => num === arrayDeNumeros[i]).length > 1
-      ) {
-        moda = arrayDeNumeros[i];
-      }
-    }
     const parrafo = document.getElementById("mensaje");
     if (parrafo && parrafo instanceof HTMLParagraphElement) {
-      parrafo.textContent = `El mínimo es ${minimo}, el máximo es ${maximo}, la media es ${media} y la moda es ${moda}`;
+      parrafo.textContent = `El mínimo es ${minimo}, el máximo es ${maximo}, la media es ${media} y la moda es ${laModa}`;
     }
   }
 };
